f43e94cd39f62082e3432e10fedb56296e467d3d
youbeebee <youbeebee@gmail.com>
2017-08-02 17:07:06 +0900

    [Node.js]'Express-URL을 이용한 정보의 전달'까지 Sementic URL

--- server_side_js/README.md
+++ server_side_js/README.md
@@ -44,3 +44,20 @@ Express에서 권장하는 템플릿 엔진. 쉽게 html코드를 만들 수 있
     //Express에서 사용
     app.set('view engine', 'jade');
     app.set('views', './views'); //jade 파일을 넣을 디렉토리. 생략해도 views를 기본값으로 한다.
+
+## URL을 이용한 정보의 전달
+
+http://a.com/topic?id=1
+
+[프로토콜]://[domain]/[path]?[query string]&[query string2]
+
+### Semantic URL
+URL을 의미적으로 보기 쉬운 방식으로 나타내줌(Restful api 참고)
+* Non-semantic URL : [url]?page=1&line=10
+* Semantic URL     : [url]/page/1/10
+
+사용법
+
+    app.get('/topic/:id/:mode', (req, res) => {\
+        res.send(req.params.id+','+req.params.mode)\
+    });
--- server_side_js/app.js
+++ server_side_js/app.js
@@ -13,8 +13,11 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.send('<h1>Hello homepage</h1>');
 }); //GET 방식으로 homepage에 접속한 경우
+app.get('/login', (req, res) => {
+    res.send('Login please');
+}); //GET 방식으로 ./login에 접속한 경우
 app.get('/sierra', (req, res) => {
-    res.send('Hello Seirra, <img src="/sierra.jpg" width="20%", height="40%">');
+    res.send('Hello Seirra, <img src="/sierra.jpg" width="20%", height="20%">');
 });
 app.get('/dynamic', (req, res) => {
     var lis = ''
@@ -36,19 +39,38 @@ app.get('/dynamic', (req, res) => {
     </html>`;//줄바꿈이 포함된 문자열을 사용할 수 있는 표준. 내부에선 ${}로 변수를 나타낼수 있음
     res.send(output);
 });
-app.get('/login', (req, res) => {
-    res.send('Login please');
-}); //GET 방식으로 ./login에 접속한 경우
 //Jade로 템플릿 사용
 app.get('/template', (req, res) => {
     res.render('temp', {title:'Jade', time:Date()}); //views/temp.jade
+});
+//query string
+app.get('/topic', (req, res) => {
+  var topics = [
+    'Javascript is....',
+    'Nodejs is...',
+    'Express is...'
+  ];
+  var output = `
+  <a href="/topic?id=0">JavaScript</a><br>
+  <a href="/topic?id=1">Nodejs</a><br>
+  <a href="/topic?id=2">Express</a><br><br>
+  ${topics[req.query.id]}
+  `
+  res.send(output);
+});
+app.get('/topic/:id/:mode', (req, res) => {
+    res.send(req.params.id+','+req.params.mode)
 });
 
 app.listen(3000, () => {
     console.log('Connect 3000 port!');
 });
+
 // 실습 키워드 $> node app.js
 //http://localhost:3000 으로 접속
 //http://localhost:3000/login
 //http://localhost:3000/static.html //정적인 페이지
 //http://localhost:3000/dynamic     //동적인 페이지
+//http://localhost:3000/template    //jade를 사용한 템플릿 페이지
+//http://localhost:3000/topic?id=0
+//http://localhost:3000/topic/1/edit
