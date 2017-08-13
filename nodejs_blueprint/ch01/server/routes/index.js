3717e4e014c14dfcddbd687aa71fd06b647001ed
youbeebee <youbeebee@gmail.com>
2017-08-13 11:17:41 +0900

    [Node.js]1.6 로그인, 가입, 프로필 템플릿 추가하기, 1.7 추가적인 미들웨어 설치하기

--- nodejs_blueprint/ch01/server/routes/index.js
+++ nodejs_blueprint/ch01/server/routes/index.js
@@ -6,4 +6,22 @@ router.get('/', (req, res, next) => {
     res.render('index', { title: 'Express from ./server/' });
 });
 
+/* GET 로그인 페이지 */
+router.get('/login', (req, res, next) => {
+    res.render('login', { title: 'Login Page', message: req.flash('loginMessage') });
+    //connect-flash 모듈 필요
+});
+
+/* GET 가입 페이지 */
+router.get('/signup', (req, res, next) => {
+    res.render('signup', { title: 'Signup Page', message: req.flash('signupMessage') });
+});
+
+/* GET 프로필 페이지 */
+router.get('/profile', (req, res, next) => {
+    res.render('profile', { title: 'Profile Page', user: req.user, 
+        avatar: gravatar.url(req.user.email, {s: '100', r: 'x', d: 'retro'}, true) });
+    //gravatar 모듈 필요
+});
+
 module.exports = router;
